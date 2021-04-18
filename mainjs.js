4e1738cc036cacdd7eb8ad6ae5c360ba4291e23c
Daff <316081287@pcpuma.acatlan.unam.mx>
2021-04-18 17:45:34 -0500

    frontend version 1

---
+++ mainjs.js
@@ -0,0 +1,14 @@
+'use strict'
+
+function cambiacolor(color){
+  boton.style.background = color;
+}
+
+var boton = document.querySelector("#agregar");
+
+var intro = document.querySelector(".introduccion").innerHTML;
+
+//boton.className = "hola";
+
+console.log(intro);
+
